f301eff16fb08bad1279f08782958103269720c9
Marcelo Esposito <esposito.marcelo.00@gmail.com>
2021-11-16 17:34:14 +0100

    trial 1

---
+++ greenapp/greenapp/greenappApp.swift
@@ -0,0 +1,17 @@
+//
+//  greenappApp.swift
+//  greenapp
+//
+//  Created by Marcelo Esposito on 16/11/21.
+//
+
+import SwiftUI
+
+@main
+struct greenappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
