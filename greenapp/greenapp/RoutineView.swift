8b491b637cfa5a96f06c6f771905f14a7064d3d6
Marcelo Esposito <esposito.marcelo.00@gmail.com>
2021-11-17 19:51:58 +0100

    Revert "first bozza of routine model (using class, gianluca recommends using structs and enums)"
    This reverts commit 8c6ef096283a2ad2a5a365c7f0a9ba21ebefccad.

--- greenapp/greenapp/RoutineView.swift
+++ greenapp/greenapp/RoutineView.swift
@@ -8,46 +8,11 @@
 import Foundation
 import SwiftUI
 
-
-//make identifiable
-class Routine {
-//    var id: ObjectIdentifier
-    var routineName : String
-    var icon : String
-    var category : String
-    var startHour : Int
-    var startMinute : Int
-    var endHour : Int
-    var endMinute : Int
-    var allDay: Bool
-    
-    init (routineName : String, icon : String, category : String, startHour : Int , startMinute : Int, endHour : Int, endMinute : Int, allDay : Bool){
-//        self.id = UUID()
-    self.routineName = routineName
-    self.icon = icon
-    self.category = category
-    self.startHour = startHour
-    self.startMinute = startMinute
-    self.endHour = endHour
-    self.endMinute = endMinute
-    self.allDay = allDay
-    }
-}
-
-var routines : [Routine] = [
-    Routine(routineName: "Transportation", icon: "car.fill", category: "transportation", startHour: 19, startMinute: 00, endHour: 20, endMinute: 00, allDay: false)
-    
-]
-
-
 struct RoutineView: View {
     
     var titolo: String
     
     var body: some View {
-        NavigationView{
     Text(titolo)
-                .navigationTitle(titolo)
-    }
     }
 }
